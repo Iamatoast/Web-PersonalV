f0eacd8964464f7c2505c302e1e1bb71820c45e2
Gonzalo Susel <131721641+gonzalosusel@users.noreply.github.com>
2024-08-08 11:30:30 -0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Web_PersonalV.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
