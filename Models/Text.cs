c7c884200a5f40144104a1a743da9790d4437cbb
Iamatoast <neuyv@protonmail.com>
2024-08-07 21:17:19 -0300

    Synthetik terminado

--- Models/Text.cs
+++ Models/Text.cs
@@ -51,41 +51,41 @@ public class Text
     };
     public static string[,] SynthetikTxt = new string[,] 
     {
-        // "MainTitle", "VideoLink", "Title1", "P1", "Title2", "P2", "Title3", "P3", "Title4", "TableTitle1", "TableTitle2", "Row1P1", "Row1P2", "Row2P1", "Row2P2", "Row3P1", "Row3P2", "Row4P1", "Row4P2", "Row5P1", "Row5P2", "Row6P2", "Row7P1", "Row7P2", "Row8P1", "Row8P2"
+        // "MainTitle", "VideoLink", "Title1", "P1", "Title2", "P2", "Title3", "P3", "Title4", "TableTitle1", "TableTitle2", "Row1P1", "Row1P2", "Row2P1", "Row2P2", "Row3P1", "Row3P2", "Row4P1", "Row4P2", "Row5P1", "Row5P2", "Row6P2", "Row7P1", "Row7P2", "Row8P1", "Row8P2", "Row9P1", "Row9P2", "Row10P1", "Row10P2"
         // 31 Elementos
         {
             //Normal
             "SYNTHETIK: Legion Rising",
             "https://www.youtube.com/embed/iyEZ2WyF1tE?si=HwNwgJrJmPI6VRSB",
             "Resumen",
-            "P1",
+            "Explosiones, destruccion, disparos, gasolina, muchos enemigos, explosiones; todo esto es lo que te vas a encontrar en SYNTHETIK: Legion Rising. Preparate para acribillar a hordas de robots asesinos y maquinas de guerra que estan dispuestos a matarte",
             "El juego",
-            "P2",
+            "SYNTHETIK es un rouge-lite shooter isometrico, que va a desafiar tu capacidad de manejar situaciones bajo presion, dandote horas de entretenimiento en el proceso. Te encontras en el año 1985, año en el cual los sirvientes roboticos de la corporacion Kaida, lider mundial en robotica, se revelaron en contra de los humanos, buscando destruir a estos. Sos un Androide que esta en el lado humano, y tu objetivo es derrotar a la Machine Legion de desatar al corazon de armagedon SYNTHETIK es un juego desafiante, lleno de contenido que te hara jugar por horas. Tiene una curva de apredizaje pronunciada, evidenciado por la decision de hacer que tu arma se trabe (si), que tus armas se sobrecalienten y la posibilidad de realizar un skill check para recargar mas rapido. El juego tiene una gran variedad de enemigos dificiles a los que enfrentar con tu gran arsenal de armas (al igual que municiones) y gadgets. Todas las partidas son unicas, no solo por la gran cantidad de formas de matar al enemigo que tenes, si no que tambien diferentes clases, las cuales cambian completamente tu forma de jugar al juego. Todo esto combinado con un muy buen art style hace que SYNTHETIK sea un excelente juego",
             "Aspectos para mejorar",
-            "P3",
+            "Aunque el juego sea muy bueno, eso no significa que existan cosas a mejorar, principalmente la optimizacion de este, ya que, debido a la gran cantidad de efectos en pantalla, puede que tus fps se reduzcan. ",
             "Descripcion mas Detallada",
             "Aspecto",
-            "Comentario",
+            "Muy buen art style, los enemigos son reconocibles y lindos de ver",
             "Gameplay",
-            "Row1P2",
+            "Un muy buen rougelite que tiene una gran variedad de formas de enfrentarte a los enemigos. El gameplay es suficientemente complejo para que no sea aburrido y suficientemente directo para que cualquiera pueda probarlo",
             "Precio",
-            "Row2P2",
+            "El precio vale la pena por la cantidad de horas que le podes meter al juego con una sola clase (hay 8)",
             "Dificultad",
-            "Row3P2",
+            "No existen modos de dificultad en si, pero podes seleccionar si ir a un aera en alerta (mas dificil) o un aera mas tranquila (mas facil) y maldiciones, las cuales hacen mas dificil tu experiencia de juego, lo que es bueno ya que tenes opciones para elegir. La dificultad en si escala a medida que vas avanzando, y lo hace de manera veloz, no extremadamente alta pero se nota.",
             "Gráficos",
-            "Row4P2",
+            "Un juego isometrico, con una ambientacion de ciencia ficcion. No hay perdida de calidad de la imagen en el juego. Es reconocible y no se vuelve repetitivo.",
             "Historia",
-            "Row5P2",
+            "No es la gran cosa, tiene una linda ambientacion, pero el juego lo venis a jugar por el gameplay.",
             "Efectos",
-            "Row6P2",
+            "Muy buenos efectos, Micheal Bay llora, sentis la intensidad de cada una de tus batallas. Tambien tienen muy buenos efectos de ambiente, como niebla y luces, aunque puede que todo esto resulte en una caida de fps (de todos modos se soluciona con una gpu decente, te odio graficos integrados)",
             "Música",
-            "Row7P2",
+            "Muy buena musica, te sirve para ponerte en los zapatos de tu personaje, especialmente gracias a musica adaptativa, la cual va cambiando de intensidad segun la situacion en la que te encontras",
             "Audiencia",
-            "Row8P2",
+            "Toda persona fanaticas de los rouge-like le va a gustar, y para las personas que no les gusta perder progreso, este juego tiene una meta progresion, alias, vas progresando a medida que jugas, desbloqueando armas, gadgets y/o subiendo de nivel al finalizar una partida (sea perdiendo o ganando). Definitivamente es un juego que las personas que les guste el grindeo (no es necesario) van a disfrutar y jugadores de shooters tacticos (como CS:GO o Valorant) pueden tranquilamente disfrutar de este juego",
             "Requerimientos de PC",
-            "Row9P2",
-            "Bugs & Problemas",
-            "Row10P2"
+            "Algunos bajones de frames leves pero nada del otro mundo, una computadora decente corre el juego sin problema",
+            "En que plataformas se encuentra",
+            "GOG.com; Steam; itch.io; Nintendo eShop; Playstation Store; Xbox"
         },
         {
             //Pootis
